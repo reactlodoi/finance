8e6005adafb7e45528b473d462e6a9c993dbab20
reactlodoi <react.lodoi@gmail.com>
2023-01-24 14:17:39 +0800

    finance module iig hj ehelsen. exp, inc objectuudig uusgesen.

--- app.js
+++ app.js
@@ -1,13 +1,66 @@
 // delgetstei ajillah controller
-var uiController = (function () {})();
+var uiController = (function () {
+
+  // html,css tei holbootoi uurchlultund ortoj boloh ugugduluudig neg gazar hamgaalav
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  // public services: iife ashiglan module archt aar hj bga uyed public service uude return eer damjuuldag
+  return {
+    getInput: function () {
+      return {
+        // DOM oosoo select hj bn
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 // Sanhuutei ajillah controller
-var financeController = (function () {})();
+var financeController = (function () {
+  var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      inc: 0,
+      exp: 0
+    }
+  };
+
+  // data.allItems.inc.push(100);
+
+
+
+})();
 
 // program holbogch controller
 var appController = (function (uiController, financeController) {
+
   var ctrlAddItem = function () {
-    console.log("daraglaa...");
+    console.log(uiController.getInput());
     // 1. oruulah ugugdlig delgetsees olj avna
     // 2. olj avsan ugugdluude sanhuugin controllert damjuulj tend hadgalna.
     // 3. olj avsan ugugdluudee web deer ni tohiroh hesegt gargana.
@@ -15,16 +68,32 @@ var appController = (function (uiController, financeController) {
     // 5. delgetsend etssiin uldegdel tootsoog gargana.
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
+  var setupEventListeners = function (){
 
-  document.addEventListener("keypress", function (event) {
-    // which ni deer uyiin browseruuded
-    if (event.keyCode === 13 || event.which === 13) {
+    var DOM = uiController.getDOMstrings();
+
+      document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function (event) {
+      // which ni deer uyiin browseruuded
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  }
+
+  return {
+    init: function (){
+        console.log("start app...");
+        setupEventListeners();
     }
-  });
+  }
+
+  
 })(uiController, financeController);
 
 // ctrl + shift + j = console
+
+appController.init();
