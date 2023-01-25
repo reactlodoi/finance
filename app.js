2c0b1e28e6c5a3e362553595456955cad3034c9f
reactlodoi <react.lodoi@gmail.com>
2023-01-25 10:13:56 +0800

    finance module iin tusviig tootsoolj duusav. odoo ui deer gargana.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var uiController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
   };
 
   // public services: iife ashiglan module archt aar hj bga uyed public service uude return eer damjuuldag
@@ -16,23 +18,52 @@ var uiController = (function () {
         // DOM oosoo select hj bn
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,
+        // user ees utga avahdaa too ruu parseInt() gsen method oor avna.
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value),
       };
     },
     getDOMstrings: function () {
       return DOMstrings;
     },
+
+    clearFields: function (){
+      // olon elementig DOM oos barij avahdaa querySelectorAll method iig ashiglana. butsaagaad bidend
+      // massive bish list data structure butsaadag.
+      var fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+      // convert list to array. slice funkts ni dotor ni array bichihed tuhain arrayg butsaadag. 
+      // gevch list-d slice gsen funkts bdaggu uchir call funkts ashiglaj funktsig ni
+      // zeelj array butsaaj, array luu hurvuulev.
+      var fieldArr = Array.prototype.slice.call(fields);
+
+      // field bga buh elementudig element bolgonii huvid davtaltaar hooslov.
+      // hiih uildelig ni nergu funktseer damjuulj ugnu gurvan argument dotroo avdag.
+      // function (<element><tuhain element ni yamar indextei ve><uurig ni butsaj ugnu>)
+      fieldArr.forEach(function(el, index, array){
+          el.value = "";
+          // el.value = index gevel tuhain indexig ni utgand ugnu
+      });
+
+      // odoo manai array dotor 2 element bga ehnii desc, value bga.
+      // ene hoyroin cursor maani desc deer arilsni daraa bhad tohiruulahin tuld focus method iig duudna.
+      fieldArr[0].focus();
+
+      // for(var i = 0; i<fieldArr.length; i++){
+      //   fieldArr[i].value = "";
+      // }
+    },
+
     // item ni bidnii medeelel, type ni orlogo/zarlaga
     addListItem: function (item, type){
     
       // orlogo zarlagiig element aguulsan html- iig  beltgene.
       var html, list;
       if(type === "inc"){
-        list = ".income__list";
+        list = DOMstrings.incomeList;
         html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESC$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       else {
-        list = ".expenses__list";
+        list = DOMstrings.expenseList;
         html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESC$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
@@ -60,6 +91,14 @@ var financeController = (function () {
     this.value = value;
   }
 
+  var calculateTotal = function(type){
+      var sum = 0;
+      data.items[type].forEach(function(el){
+        sum = sum + el.value; // zovhon el geed nemcihvel NaN buyu Not a number gsen ugnii tovchlol aldaa garch boldog
+      })
+      data.totals[type] = sum;
+  };
+
   var data = {
     items: {
       inc: [],
@@ -68,12 +107,38 @@ var financeController = (function () {
     totals: {
       inc: 0,
       exp: 0
-    }
+    },
+    tusuv: 0,
+    huvi: 0
   };
 
   // data.items.inc.push(100);
 
   return {
+    tusuvTsooloh: function(){
+
+        // niit orlogin niilberig bodno
+        calculateTotal("inc");
+
+        // niit zarlagiig bodno
+        calculateTotal("exp");
+
+        // orlogo zarlagin zoruug bodno
+        data.tusuv = data.totals.inc - data.totals.exp;
+
+        // orlogo zarlagin huvig bodno
+        data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100); 
+    },
+    tusviigAvah: function(){
+      // data-g dangaar ni yvuulbal turshlaggu programistuud uurchilcihnu.
+      // tiimes finance module iin buur etssin ur dung ni l yvuulah heregtei
+        return {
+          tusuv: data.tusuv,
+          huvi: data.huvi,
+          totalInc: data.totals.inc,
+          totalExp: data.totals.exp
+        }
+    },
     // user iin garaas avsan utguudig object dotor hadgalav. type ruu exp, esvel inc orj irne.
     addItem: function (type, desc, val){
 
@@ -103,12 +168,20 @@ var appController = (function (uiController, financeController) {
     var ctrlAddItem = function () {
   // 1. oruulah ugugdlig delgetsees olj avna
     var input = uiController.getInput();
-    // 2. olj avsan ugugdluude sanhuugin controllert damjuulj tend hadgalna.
-    var item = financeController.addItem(input.type, input.description, input.value);
-    // 3. olj avsan ugugdluudee web deer ni tohiroh hesegt gargana.
-    uiController.addListItem(item, input.type);
-    // 4. tusviig tootsolno
-    // 5. delgetsend etssiin uldegdel tootsoog gargana.
+    
+    if(input.description !== "" && input.value !== ""){
+       // 2. olj avsan ugugdluude sanhuugin controllert damjuulj tend hadgalna.
+      var item = financeController.addItem(input.type, input.description, input.value);
+      // 3. olj avsan ugugdluudee web deer ni tohiroh hesegt gargana.
+      uiController.addListItem(item, input.type);
+      uiController.clearFields();
+      // 4. tusviig tootsolno
+      financeController.tusuvTsooloh();
+      // 5. etssiin uldegdel 
+      var tusuv = financeController.tusviigAvah();
+      // 6. delgetsend tusviin tootsoog gargana.
+      console.log(tusuv);
+    }
   };
 
   var setupEventListeners = function (){
